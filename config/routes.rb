213866d2b4addb8448c8ef329f0e9c0f6c92417e
Hannah Malm <hannahemalm616@gmail.com>
2021-08-25 06:32:55 -0400

    commit tree 9acd9e68a1652d0e01bdf7d970c52e89ab2cfd1a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'about', to: 'static#about'
+  #HTTP verb - get
+  #path - about represents the path in URL bar that the route is mapped to
+  #controller - static#acout - tells Rails routing system that this route should be passed through static controllers about action
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
